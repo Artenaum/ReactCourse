4e2e39dc660b159d4934fba589d49f323ccf1f9f
Artenaum <Artenaum@gmail.com>
2025-10-14 18:06:38 +0300

    Sort and Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-import React from "react"
-import { useState } from "react"
+import { useState, useMemo } from "react"
 import './styles/App.css'
-import PostItem from "./components/PostItem"
 import PostList from "./components/PostList"
+import PostForm from "./components/PostForm"
+import PostFilter from "./components/PostFilter"
 
 function App() {
 	const [posts, setPosts] = useState([
-		{id: 1, title: 'Javascript', body: 'Description'},
-		{id: 2, title: 'Javascript 2', body: 'Description'},
-		{id: 3, title: 'Javascript 3', body: 'Description'}
+		{id: 1, title: 'аа', body: 'бб'},
+		{id: 2, title: 'гг 2', body: 'аа'},
+		{id: 3, title: 'вв 3', body: 'яя'}
 	])
-	const [posts2, setPosts2] = useState([
-		{id: 1, title: 'Python', body: 'Description'},
-		{id: 2, title: 'Python 2', body: 'Description'},
-		{id: 3, title: 'Python 3', body: 'Description'}
-	])
+	const [filter, setFilter] = useState({sort: '', query: ''})
+
+	const sortedPosts = useMemo(() => {
+		if (filter.sort) {
+			return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+		}
+		return posts
+	}, [filter.sort, posts])
+
+	const sortedAndSearchedPosts = useMemo(() => {
+		return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+	}, [filter.query, sortedPosts])
+
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost])
+	}
+
+	const removePost = (post) => {
+		setPosts(posts.filter(p => p.id !== post.id))
+	}
 
 	return (
 		<>
 			<div className="App">
-				<PostList posts={posts} title="Посты про JS"/>
-				<PostList posts={posts2} title="Посты про Python"/>
+				<PostForm create={createPost}/>
+				<hr style={{margin: '15px 0'}}/>
+				<PostFilter
+					filter={filter}
+					setFilter={setFilter}
+				/>
+				<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
 			</div>
 		</>
   	)
